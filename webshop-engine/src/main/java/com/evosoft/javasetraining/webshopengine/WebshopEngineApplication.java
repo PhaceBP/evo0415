f24691063e1444441024b4eeae74b07431fccf9b
Attila Balogh-Biró <attila.balogh86@gmail.com>
2019-04-23 13:23:56 +0200

    add webshop engine

---
+++ webshop-engine/src/main/java/com/evosoft/javasetraining/webshopengine/WebshopEngineApplication.java
@@ -0,0 +1,13 @@
+package com.evosoft.javasetraining.webshopengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopEngineApplication.class, args);
+	}
+
+}
